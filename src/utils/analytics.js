4231d5a25284d5afce14b6f5bdca277212d1c690
Darko41 <kracunovicd@icloud.com>
2025-11-01 17:26:15 +0100

    Added Google Analytics

---
+++ src/utils/analytics.js
@@ -0,0 +1,21 @@
+export const GA_MEASUREMENT_ID = 'G-K5FV28D943';
+
+// Track page views
+export const pageview = (url) => {
+  if (typeof window.gtag !== 'undefined') {
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    });
+  }
+};
+
+// Track events
+export const event = ({ action, category, label, value }) => {
+  if (typeof window.gtag !== 'undefined') {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+};
